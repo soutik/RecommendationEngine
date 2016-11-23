59a6b4a90b6b02348cbfa267d7bd5ffa89f0677e
Soutik Chakraborty <schakraborty@payoff.com>
2016-11-23 14:05:08 -0800

    minor fix

--- myapps/python/server.py
+++ myapps/python/server.py
@@ -7,7 +7,7 @@ def init_spark_context():
     # load spark context
     conf = SparkConf().setAppName("movie_recommendation-server")
     # IMPORTANT: pass aditional Python modules to each worker
-    sc = SparkContext(conf=conf, pyFiles=['engine.py', 'app.py'])
+    sc = SparkContext(conf=conf, pyFiles=['/root/RecommendationEngine/myapps/python/engine.py', '/root/RecommendationEngine/myapps/python/app.py'])
  
     return sc
  
