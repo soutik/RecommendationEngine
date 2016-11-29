d65f1b8762181d84aff9e7ffd8bb2d1fcda6c8c8
Soutik Chakraborty <schakraborty@payoff.com>
2016-11-29 01:47:17 -0800

    removed non-functioning spark worker

--- bin/start-all-services.sh
+++ bin/start-all-services.sh
@@ -8,8 +8,8 @@ nohup $SPARK_HOME/sbin/start-master.sh --webui-port 6060 -h 0.0.0.0 &
 echo '...Starting Spark Worker 1...'
 nohup $SPARK_HOME/sbin/start-slave.sh --cores 8 --memory 48g --webui-port 6061 -h 0.0.0.0 spark://127.0.0.1:7077 &
 
-echo '...Starting Spark Worker 1...'
-nohup $SPARK_HOME/sbin/start-slave.sh --cores 8 --memory 48g --webui-port 6062 -h 0.0.0.0 spark://127.0.0.1:7077 &
+#echo '...Starting Spark Worker 1...'
+#nohup $SPARK_HOME/sbin/start-slave.sh --cores 8 --memory 48g --webui-port 6062 -h 0.0.0.0 spark://127.0.0.1:7077 &
 
 echo '...Starting Spark History Server...'
 nohup $SPARK_HOME/sbin/start-history-server.sh &
