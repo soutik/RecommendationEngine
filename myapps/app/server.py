cbf1e79d9efadae757e026b9238b75fab1274d58
Soutik Chakraborty <schakraborty@payoff.com>
2016-11-29 01:03:00 -0800

    changed data usage to small

--- myapps/app/server.py
+++ myapps/app/server.py
@@ -36,7 +36,7 @@ def run_server(app):
 if __name__ == "__main__":
     # Init spark context and load libraries
     sc = init_spark_context()
-    dataset_path = os.path.join('datasets', 'ml-latest')
+    dataset_path = os.path.join('datasets', 'ml-latest-small')
     app = create_app(sc, dataset_path)
  
     # start web server
